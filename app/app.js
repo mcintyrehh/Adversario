d28d7f8475fe8194dab23e072e00ad3429871035
Onray <mcintyrehh@gmail.com>
2018-09-01 23:19:58 -0400

    html and styling up, started survey logic

--- app/app.js
+++ app/app.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', htmlRouter);
-// app.use('/users', usersRouter);
+app.use('/api/friends', apiRouter);
 
 module.exports = app;
 
---
+++ app/public/javascripts/survey.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    
+    function question() {
+        `<div class="survey-questions border">
+          <div class="question-number text-center">Question 1</div>
+          <div class="question text-center">Its fine to be a few minutes late if you're good friends</div>
+          <span>(strongly agree)</span>
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" id="inlineRadio1" value="1">
+            <label class="form-check-label" for="inlineRadio1">1</label>
+          </div>
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" id="inlineRadio2" value="2">
+            <label class="form-check-label" for="inlineRadio2">2</label>
+          </div>
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" id="inlineRadio3" value="3">
+            <label class="form-check-label" for="inlineRadio3">3</label>
+          </div>
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" id="inlineRadio4" value="4">
+            <label class="form-check-label" for="inlineRadio4">4</label>
+          </div>
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" id="inlineRadio5" value="5">
+            <label class="form-check-label" for="inlineRadio5">5</label>
+          </div>
+          <span>(strongly disagree)</span>
+        </div>`
+    }
+
+
+
+
+});
